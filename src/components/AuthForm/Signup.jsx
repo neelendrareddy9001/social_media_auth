f6ce0e3e73e4f7c905c6383e0eb1c4d1a9a3d923
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-12-03 12:44:58 +0100

    updated

--- src/components/AuthForm/Signup.jsx
+++ src/components/AuthForm/Signup.jsx
@@ -23,11 +23,60 @@ const Signup = () => {
       <Input
         placeholder="Email"
         fontSize={14}
+        mb={4}
+        color={"white"}
+        fontWeight={400}
+        borderRadius={"8px"}
         type="email"
         size={"sm"}
         value={Inputs.email}
         onChange={(e) => setInputs({ ...Inputs, email: e.targetvalue })}
       />
+      <Input
+        placeholder="Username"
+        fontSize={14}
+        mb={4}
+        color={"white"}
+        fontWeight={400}
+        borderRadius={"8px"}
+        type="text"
+        size={"sm"}
+        value={Inputs.userName}
+        onChange={(e) => setInputs({ ...Inputs, userName: e.target.value })}
+      />
+      <InputGroup>
+        <Input
+          placeholder="Password"
+          fontSize={14}
+          mb={4}
+          color={"white"}
+          fontWeight={400}
+          borderRadius={"8px"}
+          type={showPassword ? "text" : "password"}
+          value={Inputs.password}
+          size={"sm"}
+          onChange={(e) => setInputs({ ...Inputs, password: e.target.value })}
+        />
+        <InputRightElement h={"full"}>
+          <Button
+            variant={"ghost"}
+            size={"sm"}
+            onClick={() => setShwoPassword(!showPassword)}
+          >
+            {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+
+      <Button
+        w={"full"}
+        colorScheme="blue"
+        size={"sm"}
+        fontSize={14}
+        // isLoading={loading}
+      >
+        Signup
+      </Button>
     </>
   );
 };
