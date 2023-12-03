5f537502c27a204c7c1bbf1a8de90d2f6a0ff2c7
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-12-03 17:36:20 +0100

    sidebar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePages/HomePage";
 import AuthPage from "./pages/AuthPage/AuthPage";
+import PageLayout from "./Layout/PageLayout/PageLayout";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<AuthPage />} />
-      </Routes>
+      <PageLayout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </PageLayout>
     </>
   );
 }
