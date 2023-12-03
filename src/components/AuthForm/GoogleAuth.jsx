4c37b55a6ca015f70900494673c49f4dd42103a6
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-12-03 16:23:42 +0100

    login and sign up added ternary operator

--- src/components/AuthForm/GoogleAuth.jsx
+++ src/components/AuthForm/GoogleAuth.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Flex, Image, Text } from "@chakra-ui/react";
 
-const GoogleAuth = () => {
-  return <div>GoogleAuth</div>;
+const GoogleAuth = ({ prefix }) => {
+  return (
+    <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"}>
+      <Image src="/google.png" w={5} alt="Google logo" />
+      <Text mx={2} color={"blue.500"}>
+        {prefix} with Google
+      </Text>
+    </Flex>
+  );
 };
 
 export default GoogleAuth;
