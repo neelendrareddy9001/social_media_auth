b2bdc805b3d3cb88776f446b5d95ad1f69bfde27
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-12-04 22:22:40 +0100

    updated

--- src/pages/FeedPosts/PostFooter.jsx
+++ src/pages/FeedPosts/PostFooter.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import { useState } from "react";
 
 const PostFooter = () => {
+  const [liked, setLiked] = useState(false);
+  const [likes, setLikes] = useState(1000);
+
+  const handleClick = () => {
+    if (liked) {
+      setLiked(false);
+      setLikes(liked - 1);
+    }
+  };
   return <div>PostFooter</div>;
 };
 
