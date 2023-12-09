c16ee3e6438bc3b5d1e4e74120985e16a4c2a172
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-12-09 23:26:14 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePages/HomePage";
 import AuthPage from "./pages/AuthPage/AuthPage";
 import PageLayout from "./Layout/PageLayout/PageLayout";
+import ProfilePage from "./pages/ProfilePage/ProfilePages";
+
+import useAuthStore from "./Store/authStore";
 
 function App() {
+  const authUser = useAuthStore((state) => state.user);
   return (
     <>
       <PageLayout>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/auth" element={<AuthPage />} />
+          <Route
+            path="/"
+            element={authUser ? <HomePage /> : <Navigate to="/auth" />}
+          />
+          <Route
+            path="/auth"
+            element={!authUser ? <AuthPage /> : <Navigate to="/" />}
+          />
+          <Route path="/:username" element={<ProfilePage />} />
         </Routes>
       </PageLayout>
     </>
---
+++ src/pages/ProfilePage/ProfilePages.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfilePages = () => {
+  return <div>ProfilePages</div>;
+};
+
+export default ProfilePages;
