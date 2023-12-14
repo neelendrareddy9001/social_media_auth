5a7182b8a7ee48898e0734bc42c14f362e6ea418
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-12-14 23:08:10 +0100

    updated google auth

--- src/components/AuthForm/GoogleAuth.jsx
+++ src/components/AuthForm/GoogleAuth.jsx
@@ -1,6 +1,21 @@
 import { Flex, Image, Text } from "@chakra-ui/react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase/firebase";
+import useShowToast from "../../hooks/useShowToast";
+import useAuthStore from "../../Store/authStore";
 
 const GoogleAuth = ({ prefix }) => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const showToast = useShowToast();
+  const loginUser = useAuthStore((state) => state.login);
+
+  const handleGoogleAuth = async () => {
+    const newUser = await signInWithGoogle();
+    if (!newUser && error) {
+      showToast("Error", erro);
+    }
+  };
+
   return (
     <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"}>
       <Image src="/google.png" w={5} alt="Google logo" />
