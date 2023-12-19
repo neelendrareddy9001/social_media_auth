a40bd81bbbcf408527678969c3a83f6d2d1a0971
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-12-19 23:57:41 +0100

    user profile store updated

---
+++ src/Store/userProfileStore.js
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+
+const useUserProfileStore = create((set) => ({
+  userProfile: nulll,
+  usetUserProfile: (userProfile) => set({ userProfile }),
+  //this is user to update the number of posts in the profile page
+  addPost: (post) =>
+    set((state) => ({
+      userProfile: {
+        ...state.userProfile,
+        posts: [post.id, ...state.userProfile.posts],
+      },
+    })),
+  deletePost: (postId) =>
+    set((state) => ({
+      userProfile: {
+        ...state.userProfile,
+        posts: state.userProfile.posts.filter((id) => id !== postId),
+      },
+    })),
+}));
---
+++ src/hooks/useGetUserProfileByUsername.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import useShowToast from "./useShowToast";
+import { collection, getDoc, query, where } from 'firebase/firestore';
+import { firestore } from "../firebase/firebase";
+import useuserProfileStore from 
+
+const useGetUserProfileByUsername = () => {
+  return <div>useGetUserProfileByUsername</div>;
+};
+
+export default useGetUserProfileByUsername;
