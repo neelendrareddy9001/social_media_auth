aebf003630b3e6da36495b1f52bc2d92333d5d80
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-12-20 22:52:10 +0100

    updated profile store

--- src/hooks/useGetUserProfileByUsername.js
+++ src/hooks/useGetUserProfileByUsername.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import useShowToast from "./useShowToast";
-import { collection, getDoc, query, where } from 'firebase/firestore';
+import { collection, getDoc, query, where } from "firebase/firestore";
 import { firestore } from "../firebase/firebase";
-import useuserProfileStore from 
+import useuserProfileStore from "../Store/userProfileStore";
 
 const useGetUserProfileByUsername = () => {
   return <div>useGetUserProfileByUsername</div>;
