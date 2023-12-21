d4b9e2e7523648e9d34e3898ad7e0f7d43a13603
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-12-21 23:21:13 +0100

    updated profileheader

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import {
+  Avatar,
+  AvatarGroup,
+  Button,
+  Flex,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import useUserProfileStore from "../../Store/userProfileStore";
 
 const ProfileHeader = () => {
-  return <div>ProfileHeader</div>;
+  const { userProfile } = useUserProfileStore();
+
+  return (
+    <Flex
+      gap={{ base: 4, sm: 10 }}
+      py={10}
+      direction={{ base: "column", sm: "row" }}
+    >
+      <AvatarGroup
+        size={{ base: "xl", md: "2xl" }}
+        justifySelf={"center"}
+        alignSelf={"flex-start"}
+        mx={auto}
+      >
+        <Avatar
+          src={userProfile.profilePicUrl}
+          alignItems="As a programmer logo "
+        />
+      </AvatarGroup>
+    </Flex>
+  );
 };
 
 export default ProfileHeader;
